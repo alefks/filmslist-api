03c1ae47b7d7e98713247f056b4fc60c73fd2f15
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-09-15 16:54:47 -0300

    routes created

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,37 @@
 const routes = require("express").Router();
 const FilmController = require("../controllers/FilmsController");
+const FilmMiddleware = require("../middlewares/FilmMiddlewares");
 
+// GET ALL
 routes.get("/films", FilmController.getAll);
+
+// GET BY ID
+routes.get(
+  "/films/:id",
+  FilmMiddleware.verifyID,
+  FilmController.getById
+);
+
+// CREATE
+routes.post("/films/create", FilmController.create);
+
+// UPDATE
+routes.put(
+  "/films/:id",
+  FilmMiddleware.verifyID,
+  FilmController.update
+);
+
+
+// DELETE
+routes.delete(
+  "/films/:id",
+  FilmMiddleware.verifyID,
+  FilmController.del
+);
+
+// FILTERS
+routes.get("/filterByName", FilmController.filterByName);
+routes.get("/filterAll", FilmController.filterAll);
+
+module.exports = routes;
