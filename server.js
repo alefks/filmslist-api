97a94611e5d9708cf42bb0431c6a12535cae37e3
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-09-15 16:54:14 -0300

    Fixed server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const routes = require("./src/routes/routes");
 const app = express();
 const port = process.env.DB_PORT;
 
-connectToDb();
 app.use(express.json());
 app.use(routes);
+
+connectToDb();
+
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
