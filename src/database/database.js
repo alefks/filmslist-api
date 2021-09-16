95be8b9835c9841f652dd3cae57f59d31bbc0b72
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-09-15 21:20:44 -0300

    fixed db name

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,7 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbChar = process.env.DB_CHAR;
 
-const db_uri = `mongodb+srv://${dbUser}:${dbPassword}@clusterfilmslist.${dbChar}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const db_uri = `mongodb+srv://${dbUser}:${dbPassword}@clusterfilmslist.${dbChar}.mongodb.net/db_filmslist?retryWrites=true&w=majority`;
 
 const connectToDb = () => {
   mongoose.connect(db_uri, {
