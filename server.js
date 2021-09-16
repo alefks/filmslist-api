9dae8ba4f7b85cab28f57d09e700c113f004b0a4
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-09-16 19:52:06 -0300

    cors test

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const connectToDb = require("./src/database/database");
 const routes = require("./src/routes/routes");
-
+const cors = require("cors");
 
 const app = express();
 const port = process.env.DB_PORT;
 
-app.use(express.json());
-app.use(routes);
+(async () => {
+  app.use(express.json());
 
-connectToDb();
+  // Liberar o CORS em todas as requisições
+//   app.use(cors());
 
+  // Ativar todos os pre-flights
+//   app.options("*", cors());
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+  app.use(routes);
+
+  connectToDb();
+  console.info("Succesfully connected to MongoDB Atlas!");
+
+  app.listen(port, () =>
+    console.log(`Server running: http://localhost:${port}`)
+  );
+})();
